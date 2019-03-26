f3c9dda5c4ed2ef7226d598a5c0651c89a5e5298
user <user@umvirt.com>
2019-03-26 05:41:39 +0000

    Database dump sql update

--- sql/database.sql
+++ sql/database.sql
@@ -2,42 +2,43 @@ set foreign_key_checks=0;
 #SQLDELIMETER
 CREATE TABLE `releases` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `release` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `release` varchar(10) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `release` (`release`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 #SQLDELIMETER
 CREATE TABLE `packages` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `code` varchar(255) NOT NULL,
+  `code` varchar(255) CHARACTER SET latin1 NOT NULL,
   `release` int(10) unsigned NOT NULL,
-  `sourcefile` varchar(255) NOT NULL,
-  `sourcedir` varchar(255) NOT NULL,
-  `configure` varchar(10240) NOT NULL,
-  `build` varchar(10240) NOT NULL,
-  `install` varchar(10240) NOT NULL,
+  `sourcefile` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `sourcedir` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `configure` varchar(10240) CHARACTER SET latin1 NOT NULL,
+  `build` varchar(10240) CHARACTER SET latin1 NOT NULL,
+  `install` varchar(10240) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `code` (`code`,`release`) USING BTREE,
   KEY `version` (`release`),
   CONSTRAINT `release_fk` FOREIGN KEY (`release`) REFERENCES `releases` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=361 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 #SQLDELIMETER
 CREATE TABLE `patches` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `package` bigint(20) unsigned NOT NULL,
-  `filename` varchar(255) NOT NULL,
+  `filename` varchar(255) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`id`),
   KEY `package` (`package`),
   CONSTRAINT `patch_fk_package` FOREIGN KEY (`package`) REFERENCES `packages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 #SQLDELIMETER
 CREATE TABLE `addons` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `package` bigint(20) unsigned NOT NULL,
-  `filename` varchar(255) NOT NULL,
+  `filename` varchar(255) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`id`),
   KEY `package` (`package`),
   CONSTRAINT `addon_fk_package` FOREIGN KEY (`package`) REFERENCES `packages` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 #SQLDELIMETER
 CREATE TABLE `dependances` (
   `package` bigint(20) unsigned NOT NULL,
@@ -47,17 +48,17 @@ CREATE TABLE `dependances` (
   KEY `dependances_fk_dependance` (`dependance`),
   CONSTRAINT `dependances_fk_dependance` FOREIGN KEY (`dependance`) REFERENCES `packages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `dependances_fk_package` FOREIGN KEY (`package`) REFERENCES `packages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 #SQLDELIMETER
 CREATE TABLE `commands` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `release` int(20) unsigned NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `commands` varchar(10240) NOT NULL,
-  `info` varchar(1024) NOT NULL,
+  `name` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `commands` varchar(10240) CHARACTER SET latin1 NOT NULL,
+  `info` varchar(1024) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`id`),
   KEY `release` (`release`),
   CONSTRAINT `commands_fk_release` FOREIGN KEY (`release`) REFERENCES `releases` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 #SQLDELIMETER
 set foreign_key_checks=1;
